66153dd09b3baffe21bd74b6619534e45919f2a5
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-30 14:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	let properties = Object.getOwnPropertyNames(Math);
+
+	return properties;
 }
 
 alert(allMethods());
