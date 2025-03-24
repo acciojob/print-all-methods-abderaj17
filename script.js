3851cb0826125b32ce7bcee8c3c2e2c4c9f811c1
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-24 21:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function allMethods() {
-  //write your code here
+  //write your code 
 	let properties = Object.getOwnPropertyNames(Math);
 
 	return properties.join(', ');
